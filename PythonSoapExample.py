509a1ab1aa69cfa9de8823e835929b961a5424f2
Peter Lancaster <peterlancast@gmail.com>
2015-10-14 14:06:46 -0700

    SOAP examples
    Examples of constructing SOAP clients in both python and ruby to make
    calls to Panopto's API, as well as some usage examples demosntrating
    making calls using the client.

---
+++ PythonSoapExample.py
@@ -0,0 +1,104 @@
+import hashlib
+import uuid
+#This demonstration uses the pysimplesoap (soap2py) library.
+#It may be downloaded from https://code.google.com/p/pysimplesoap/
+from pysimplesoap.client import SoapClient
+
+'''
+    function used to create auth code for SOAP requests
+    'userkey' is either user's Panopto admin's username, or the username decorated with the the external provider's instance name if it is an external admin user.
+    'servername' is the domain name of the Panopto server to make the SOAP request to (e.g. demo.hosted.panopto.com)
+    'sharedSecret' is the Application key from the provider on the Panopto Identity Provider's page.
+'''
+def generateauthcode(userkey, servername, sharedSecret):
+    payload = userkey + '@' + servername
+    signedPayload = payload + '|' + sharedSecret
+    m = hashlib.sha1()
+    m.update(signedPayload)
+    authcode = m.hexdigest().upper()
+    return authcode
+
+'''
+Create a new public ID for the user to be created.
+'''
+studentUserID = uuid.uuid1()
+
+'''
+Create a user object with information for the new user. Parameters here must be in this order to be accepted by the server
+'''
+studentUser = {'Email': "test@test.com",
+               'EmailSessionNotifications': "true",
+               'FirstName': "Student",
+               #Optional list of GUIDs as strings of the groups that the user should be a member in.
+               'GroupMemberships': ["xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"],
+               'LastName': "User",
+               #User's role on Panotpo. "None" will grant the user the sitewide 'Viewer' role.
+               'SystemRole': "None",
+                #Optional description of user.
+               'UserBio': "",
+               'UserId': str(studentUserID),
+               #Panopto login ID for student. May be anything.
+               'UserKey': "studentuser",
+               #Optional custom URL for user's settings.
+               'UserSettingsUrl': ""
+               }
+
+'''
+Admin auth info for making SOAP calls
+'''
+#username of admin user in Panopto.
+userkey = "admin"
+#password of admin user on Panopto server. Only required if external provider does not have a bounce page.
+password = "password"
+ #Instance name of external provider on Panotpo
+providername = "MyProvider"
+#Application key from provider on Panotpo
+applicationkey = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#Name of the panopto server to add the user to.
+servername = "demo.panopto.com"
+
+
+'''
+Create a SOAP client object using the
+'''
+client = SoapClient(wsdl="https://" + servername +"/Panopto/PublicAPI/4.6/UserManagement.svc?wsdl", trace=False)
+
+'''
+Generate auth code for making SOAP call using admin user info.
+'''
+authcode = generateauthcode(userkey, servername, applicationkey)
+
+'''
+ Create AuthenticationInfo object to be passed to server with SOAP call
+'''
+AuthenticationInfo = {'AuthCode':authcode, 'Password':password, 'UserKey':userkey}
+
+'''
+Soap call to create user in panopto. Result will contain user's public ID if successful.
+'''
+createUserResponse = client.CreateUser(
+    auth = AuthenticationInfo,
+    user = studentUser,
+    #Initial password for created user. This may be reset manually.
+    initialPassword = "studentpassword"
+)
+#Show response from attempt to create user.
+print createUserResponse
+
+'''
+Soap call to add a user to an internal group in Panopto.
+This will give them the permissions inherited by members
+of the group on associated folders and sessions.
+'''
+addMembersToGroupResponse = client.AddMembersToInternalGroup(
+    auth = AuthenticationInfo,
+    #public GUID of group to add members to
+    groupId = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    #Initial password for created user. This may be reset manually.
+    memberIDs = ["yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy", "zzzzzzzz-zzzz-zzzz-zzzz-zzzzzzzzzzzz"]
+)
+
+#Show response from attempt to add members to group.
+print addMembersToGroupResponse
+
+
---
+++ RunySoapExample.rb
@@ -0,0 +1,70 @@
+require 'digest/sha1'
+require 'uuid'
+require 'savon'
+
+
+
+#    function used to create auth code for SOAP requests
+#    'userkey' is either user's Panopto admin's username, or the username decorated with the the external provider's
+#     instance name if it is an external admin user.
+#    'servername' is the domain name of the Panopto server to make the SOAP request to (e.g. demo.hosted.panopto.com)
+#    sharedSecret' is the Application key from the provider on the Panopto Identity Provider's page.
+def generate_auth_code(username, servername, shared_secret)
+  payload = "#{username}@#{servername}"
+  signed_payload = "#{payload}|#{shared_secret}"
+  p signed_payload
+  (Digest::SHA1.hexdigest signed_payload).upcase
+end
+
+# '''
+# Admin auth info for making SOAP calls
+# '''
+#username of admin user in Panopto.
+userkey = "adminUser"
+#password of admin user on Panopto server. Only required if external provider does not have a bounce page.
+password = "password"
+#GUID application key from the providers page in Panopto
+applicationkey = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+#Name of the panopto server to add the user to.
+servername = "demo.panopto.com"
+
+
+# '''
+# Create a SOAP client object using the Savon soap library
+# '''
+client = Savon.client(
+    wsdl: "https://" + servername +"/Panopto/PublicAPI/4.6/UserManagement.svc?wsdl",
+    #Must specify endpoint with https, as making calls over ssl is required.
+    endpoint: "https://" + servername +"/Panopto/PublicAPI/4.6/UserManagement.svc",
+    #Need to disable ssl verification to make SOAP calls over ssl with Savon
+    :ssl_verify_mode => :none,
+    #We don't want request keys to be converted to any particular casing.
+    #The keys should be cased exactly as they appear in the API documentation
+    :convert_request_keys_to => :none,
+    #Add this namespace (here using the 'pub' identifier), as Savon does not correctly parse it out of the WSDL.
+    namespaces: {
+        "xmlns:pub" =>  "http://schemas.datacontract.org/2004/07/Panopto.Server.Services.PublicAPI.V40"
+    }
+)
+
+#
+# Generate auth code for making SOAP call using admin user info.
+#
+authcode = generate_auth_code(userkey, servername, applicationkey)
+
+#
+# Create AuthenticationInfo object to be passed to server with SOAP call
+#
+authentication_info = {
+    'pub:AuthCode' => authcode, #All names of object members should have the name of the custom namespace added above appended as a prefix.
+    'pub:Password' => password,
+    'pub:UserKey' => userkey
+}
+
+#Make call to ListGroups using authentication info.
+#All object names should have 'tns' appended as a prefix.
+client.call(:list_groups, message: {'tns:auth' => authentication_info})
+
+
+
+
